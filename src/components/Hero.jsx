4ff0243b3d6664952ce9361b49bc155e187aa456
Morgan <morganfavour85@gmail.com>
2025-10-02 18:54:27 +0100

    fixed overflow

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 
 const Hero = () => {
   return (
-    <main className="relative w-full min-h-[100vh] flex items-center justify-center text-center">
+   <main className="relative w-full min-h-[100svh] flex items-center justify-center text-center">
       {/* Background video */}
       <video
         autoPlay
